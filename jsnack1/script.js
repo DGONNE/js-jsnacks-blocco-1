fbb13e8c845c47ea3debff66c7a591d4a40013e5
Davide Gonnelli <davide.gonnelli1@gmail.com>
2024-12-03 11:57:07 +0100

    ES1

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,3 +1,10 @@
-const numb1 = prompt("inserisci numero")
-const numb2 = prompt("inserisci numero")
-console.log(Math.max(numb1, numb2));
+const numb1 = Number(prompt("inserisci il primo numero"));
+const numb2 = Number(prompt("inserisci il secondo numero"));
+
+if (numb1 > numb2) {
+    alert(numb1)
+} else if (numb2 > numb1) {
+    alert(numb2)
+} else {
+    alert("i numeri sono uguali")
+}
